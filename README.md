6469a1074611699c0d8fec5d25aab6f3343f9935
Sazzadur Raman <sazzadur@revinr.com>
2025-03-07 12:11:23 +0600

    using funtion

--- README.md
+++ README.md
@@ -39,3 +39,12 @@ while i < len(numbers):
 
 print("Total sum:", total)
 ```
+### 4. Functions
+```python
+n = str(input("Enter your name:"))
+
+def greet(name):
+    return f"Hello, Nice to meet you, {name}!"
+
+print(greet(n))
+```
--- test.py
+++ test.py
@@ -1,26 +1,6 @@
-# age = int(input("Inter your age:"))
-# if age >= 18:
-#     print("You are an adult.")
-# else:
-#     print("You are a minor.")
+n = str(input("Enter your name:"))
 
-##Loops
-numbers = [10, 20, 30, 40, 50]
+def greet(name):
+    return f"Hello, Nice to meet you, {name}!"
 
-total = 0
-
-for num in numbers:
-    total += num
-
-print("Total sum for :", total)
-
-numbers = [10, 20, 30, 40, 60]
-
-total = 0
-i     = 0
-
-while i < len(numbers):  
-    total += numbers[i]
-    i += 1
-
-print("Total sum while:", total)
+print(greet(n))
